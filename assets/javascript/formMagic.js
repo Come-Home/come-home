aaa9f89c13353aa0eefc837c223e06946bb6dceb
Nicolas Brent Ellerbe <nbrente@gmail.com>
2018-03-07 21:32:35 -0500

    added ajax call to long lat api

--- assets/javascript/formMagic.js
+++ assets/javascript/formMagic.js
@@ -1,25 +1,33 @@
-
-
-
 function FormApiPull(number, name, city, zipcode) {
-  var numSet = number;
-  var nameSet = name.split(" ").join("+");
-  var citySet = city.split(" ").join("+");
-  var zipSet = zipcode;
-  url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+  var numSet = number.split(" ").join("+") + ',';
+  var nameSet = name.split(" ").join("+") + ',+';
+  var citySet = city.split(" ").join("+") + ',+';
+  var zipSet = zipcode.split(" ").join("+");
+  address = 'https://maps.googleapis.com/maps/api/geocode/json?address=' +
     numSet +
-    nameSet.replace(" ", "+") +
-    citySet.replace(" ", "+") +
-    'NC' +
+    nameSet +
+    citySet +
+    'NC+' +
     zipSet +
     '&key=AIzaSyCF_LnSPL7yY5VIDbPCbBo9e03StuCuTTs';
-  console.log(url);
+  console.log(address);
+  return address;
 }
-  
-$('#entireForm').on('submit', function () {
+
+$(document).on('submit', '#entireForm', function (event) {
+  event.preventDefault();
   var number = $('#lostNumAddInput').val();
   var name = $('#lostNameAddInput').val();
   var city = $('#lostCityAddInput').val();
   var zip = $('#lostZipAddInput').val();
   FormApiPull(number, name, city, zip);
+
+  $.ajax({
+    url: address,
+    success: function (response) {
+      console.log(response.results[0].geometry.location);
+    }
+  })
+
+
 })
