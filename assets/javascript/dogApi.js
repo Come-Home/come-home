c19f23d9ebe6685aa6db7c1a4f83274ddab0e9c9
nh nc <nhnc11@yahoo.com>
2018-03-10 14:18:13 -0500

    dog

--- assets/javascript/dogApi.js
+++ assets/javascript/dogApi.js
@@ -1,13 +1,16 @@
 $('document').ready(function name(params) {
-  
-}
-$.ajax({
-    url: address,
-    //on callback response...
-    success: function (response) {
-        //...save coordinates to address
+        var address = "https://dog.ceo/api/breeds/list"
+        $.ajax({
+            url: address,
+            //on callback response...
+            success: function(response) {
+                console.log(response);
+                //...save coordinates to address
+                // var dogBreed = [];
+                //for (var i=0; i < dogBreed.length; i++) {
+                //append
+                //}
+            }
+        })
     }
-  })
 )
-
-
