1e1ba3ce5703790b96a61f56e648ca76f05c7397
earyzhikova <elenaryzh55@gmail.com>
2018-03-12 16:38:14 -0400

    Changes - LostList, Magic.js, aboutUs

--- assets/javascript/magic.js
+++ assets/javascript/magic.js
@@ -116,7 +116,9 @@ $(document).on("submit", "#entireForm", function (event) {
 
 dataRef.ref().on("child_added", function (childSnapshot) {
   if (isPageFoundLostPet) {
-    // Log everything that's coming out of each snapshot
+
+
+    // Log everything that's coming out of snapshot
     console.log(childSnapshot.val().petName);
     console.log(childSnapshot.val().imgURL);
     console.log(childSnapshot.val().breed);
@@ -133,44 +135,34 @@ dataRef.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val().city);
     console.log(childSnapshot.val().state);
     console.log(childSnapshot.val().zipcode);
-    //anytime something changes updates json array
-    dataRef.ref().on('value', function (snapshot) {
-      UpdateSnapshot(snapshot);
-    });
-    // This function updates a localy stored info in JSON and updates whenever anything changes
-    function UpdateSnapshot(snapshot) {
-      json = snapshot.toJSON();
-    }
 
+    
     var petDisplay =
       `<li>
+        <div class="collapsible-header row z-depth-5 no-margin">
 
-      <div class="collapsible-header petRecord">
-        <table>
-        <tbody>
-          <td><img class="responsive-img z-depth-3 circle lostDogImage" src="${childSnapshot.val().imgURL}"/></td>
-          <td class="petName-ld">${childSnapshot.val().petName}</td>
-          <td class="breed-ld">${childSnapshot.val().breed}</td>
-          <td class="petAge-ld">${childSnapshot.val().petAge}</td>
-          <td class="petDateLost-ld>${childSnapshot.val().petDateLost}</td>
-          <td class="comment-ld">${childSnapshot.val().comment}</td>
-          <td><button class=".pet_owner light-green darken-3 white-text" >Location Last Seen</button></td>
+          <img class="responsive-img col s2 lostDogImage z-depth-5" src="${childSnapshot.val().imgURL}"/>
+          <div class="petName-ld col s1">${childSnapshot.val().petName}</div>
+          <div class="breed-ld col s1">${childSnapshot.val().breed}</div>
+          <div class="petAge-ld col s2">${childSnapshot.val().petAge}</div>
+          <div class="petDateLost-ld col s2">${childSnapshot.val().petDateLost}</div>
+          <div class="comment-ld col s2">${childSnapshot.val().comment}</div>
+          <div class= "lostDogMap col s2"><button class="light-green darken-3 white-text ">Location</button></div>
 
-      </div>
-      <div class="collapsible-body ownerRecord">
-        <table>
-        <td class="contactFirstName-ld">${childSnapshot.val().firstName}</td>
-        <td class="contactLastName-ld">${childSnapshot.val().lastName}</td>
-        <td class="phoneNumber-ld">${childSnapshot.val().phoneNumber}</td>
-        <td class="email-ld">${childSnapshot.val().email}</td>
-      </div>
-    </li>`;
+        </div>
+        <div class="collapsible-body row no-border">
 
+          <div class="contactFirstName-ld col s3">${childSnapshot.val().firstName}</div>
+          <div class="contactLastName-ld col s3">${childSnapshot.val().lastName}</div>
+          <div class="phoneNumber-ld col s3">${childSnapshot.val().phoneNumber}</div>
+          <div class="email-ld col s3">${childSnapshot.val().email}</div>
+
+        </div>
+      </li>`
 
 
     // full list of items to the well
-    $("#petList").prepend(petDisplay)
-
+    $("#petList").prepend(petDisplay);
 
     // Handle the errors
   }
